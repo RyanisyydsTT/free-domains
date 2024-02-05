58a198e29041118edd585bad379be2ed160577e0
Ryanisyyds <129717677+RyanisyydsTT@users.noreply.github.com>
2024-02-05 15:32:43 +0800

    Update telegram.is-an.app.js

---
+++ domains/telegram.is-an.app.js
@@ -0,0 +1,13 @@
+addSubDomain({
+  description: 'I am Ryan, a Telegram bot developer, this subdomain is for my projects, thank you.',
+  domain: 'is-an.app',
+  subdomain: 'telegram',
+  owner: {
+    repo: '<https://github.com/RyanisyydsTT/QkmaoTG>',
+    email: '<ryantseng123@go.edu.tw>',
+  },
+  record: {
+    A: ['170.39.76.35'],
+  },
+  proxy: false, // Enable Cloudflare proxy
+});
